62665841be0d7b875359f5e6f457cba7ece6499b
ankitarora54 <ankitarora54@live.com>
2026-03-25 21:39:25 +0530

    added vercel and render

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -1,1 +1,2 @@
-import axios from 'axios'; export const api = axios.create({ baseURL:'http://localhost:5000/api' });
+// import axios from 'axios'; export const api = axios.create({ baseURL:'http://localhost:5000/api' });
+import axios from 'axios'; export const api = axios.create({ baseURL:'https://ai-reporting-studio-option-b.onrender.com/api' });
